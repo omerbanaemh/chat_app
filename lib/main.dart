0337160c8013ab117faa6d35ff354ba077c282c3
omerbanaemh <111679865+omerbanaemh@users.noreply.github.com>
2025-06-12 20:49:53 +0300

    chat_app

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/helper/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:chat_app/pages/chat_page.dart';
+import 'package:chat_app/pages/login_page.dart';
+import 'package:chat_app/pages/resgister_page.dart';
+
+
+void main() async  {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ScholarChat());
+}
+
+class ScholarChat extends StatelessWidget {
+  const ScholarChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+       Routes.loginPage: (context) => const LoginPage(),
+       Routes.registerPage: (context) => const RegisterPage(),
+       Routes.chatPage : (context) => ChatPage()
+      },
+      initialRoute: Routes.loginPage,
+    );
+  }
+}
